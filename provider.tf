1ad62414d057ea7ec9dc85965ffd2c74d4d5e472
cybernetic.seclusion <cybernetic.seclusion@protonmail.com>
2023-08-07 06:40:42 +0800

    updated versions

--- provider.tf
+++ provider.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_version = "~> 1.3.9"
   required_providers {
     newrelic = {
       source  = "newrelic/newrelic"
-      version = "~> 2.14.0"
+      version = "~> 3.26.0"
     }
   }
 }
